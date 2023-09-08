e7aef05b264053412673d81e04c9c00140779b51
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2023-09-08 14:45:21 +0200

    Update CLUI.js

--- CLUI.js
+++ CLUI.js
@@ -4,42 +4,54 @@ function displayCsvInTable(csvContent, contactListId, platformClient) {
 
     const rows = csvContent.split('\n').map(row => row.split(',').map(cell => cell.replace(/"/g, '')));
     const headers = rows[0];
+    const doubleId = rows[1][headers.indexOf("inin-outbound-id")] === rows[1][headers.indexOf("id")];
 
     const table = document.createElement('table');
-
     const thead = document.createElement('thead');
     const headerRow = document.createElement('tr');
+
     headers.forEach(header => {
         const th = document.createElement('th');
         th.textContent = header;
         headerRow.appendChild(th);
     });
+
     thead.appendChild(headerRow);
     table.appendChild(thead);
 
     const tbody = document.createElement('tbody');
+
     for (let i = 1; i < rows.length; i++) {
         const row = document.createElement('tr');
+        const originalValues = rows[i];
+
         rows[i].forEach((cell, index) => {
             const td = document.createElement('td');
+
             if (headers[index] === 'inin-outbound-id' || index > headers.indexOf("ContactCallable")) {
                 td.textContent = cell;
             } else {
                 const input = document.createElement('input');
                 input.value = cell;
+
                 input.addEventListener('input', () => {
                     document.getElementById('saveButton').style.display = 'block';
-                    editedRows.add(row);
+                    editedRows.add({row: row, original: originalValues});
                 });
+
                 td.appendChild(input);
             }
+
             row.appendChild(td);
         });
+
         tbody.appendChild(row);
     }
+
     table.appendChild(tbody);
 
     const bodyContent = document.body.children;
+
     for (let i = bodyContent.length - 1; i >= 0; i--) {
         if (bodyContent[i].tagName !== 'H1') {
             document.body.removeChild(bodyContent[i]);
@@ -53,50 +65,51 @@ function displayCsvInTable(csvContent, contactListId, platformClient) {
 
     const backButton = document.createElement('button');
     backButton.textContent = 'Back to contact lists';
-    backButton.addEventListener('click', () => {
-        document.location.reload();
-    });
+    backButton.addEventListener('click', () => document.location.reload());
     buttonsContainer.appendChild(backButton);
 
     const saveButton = document.createElement('button');
     saveButton.textContent = 'Save';
     saveButton.style.display = 'none';
     saveButton.id = 'saveButton';
+
     saveButton.addEventListener('click', () => {
-        editedRows.forEach(editedRow => {
+        editedRows.forEach(item => {
+            const editedRow = item.row;
+            const original = item.original;
             const cells = Array.from(editedRow.children);
-            const contactId = cells[headers.indexOf("inin-outbound-id")].textContent.trim();
-            
-            let callableValue = cells[headers.indexOf("ContactCallable")].firstChild;
-            callableValue = (callableValue && callableValue.tagName === 'INPUT') ? callableValue.value : callableValue.textContent.trim();
-    
+            const contactId = cells[headers.indexOf("inin-outbound-id")].textContent;
+
             let body = {
                 "contactListId": contactListId,
                 "data": {},
-                "callable": callableValue === "1"
+                "callable": cells[headers.indexOf("ContactCallable")].firstChild.value === "1"
             };
-    
+
+            if (doubleId) {
+                body.id = contactId;
+            }
+
             cells.forEach((cell, index) => {
-                if (index !== headers.indexOf("inin-outbound-id") && index !== headers.indexOf("ContactCallable")) {
-                    let value = (cell.firstChild && cell.firstChild.tagName === 'INPUT') ? cell.firstChild.value : cell.textContent.trim();
-                    body.data[headers[index]] = value;
+                const currentValue = cell.firstChild ? cell.firstChild.value : cell.textContent;
+
+                if (index !== headers.indexOf("inin-outbound-id") && index !== headers.indexOf("ContactCallable") && original[index] !== currentValue) {
+                    body.data[headers[index]] = currentValue;
                 }
             });
-    
+
             apiInstance.putOutboundContactlistContact(contactListId, contactId, body)
-                .then((data) => {
-                    console.log(`putOutboundContactlistContact success! data: ${JSON.stringify(data, null, 2)}`);
-                })
-                .catch((err) => {
+                .then(data => console.log(`putOutboundContactlistContact success! data: ${JSON.stringify(data, null, 2)}`))
+                .catch(err => {
                     console.log("There was a failure calling putOutboundContactlistContact");
                     console.error(err);
                 });
         });
+
         alert('All changes saved!');
     });
 
     buttonsContainer.appendChild(saveButton);
-
     document.body.appendChild(buttonsContainer);
     document.body.appendChild(table);
 }
