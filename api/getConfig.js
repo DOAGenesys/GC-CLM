b7c393a6c7526fac2ff565beb6cd14e36693d503
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2023-09-11 14:28:36 +0200

    Update getConfig.js

--- api/getConfig.js
+++ api/getConfig.js
@@ -1,7 +1,4 @@
 module.exports = (req, res) => {
-
-    console.log('Value of redirectUri:', process.env.REDIRECT_URI);
-    
     res.json({
         clientId: process.env.GC_OAUTH_CLIENT_ID,
         redirectUri: process.env.REDIRECT_URI
